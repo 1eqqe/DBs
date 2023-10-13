c6d504f313e7b657d44cfd34bd48f69249709e2c
Baidali <118974770+1eqqe@users.noreply.github.com>
2023-10-13 14:32:01 +0600

    Add files via upload

---
+++ lab2/publishing_house.sql
@@ -0,0 +1,28 @@
+31,Ward and Lock                 ,London              
+32,AST                           ,Paris               
+33,KAZAKH                        ,Semei               
+34,AST                           ,Moscow              
+35,Alpine                        ,United Kingdom      
+36,Ecsmo                         ,Ireland             
+37,Olympia Press                 ,France              
+38,Cape & Smith                  ,USA                 
+39,Random House                  ,USA                 
+40,Harcourt Brace                ,USA                 
+41,Collectors Librabry Editions  ,United Kingdom      
+42,Word                          ,Russia              
+43,Wolf                          ,Italy               
+44,Samizdat                      ,United Kingdom      
+45,Benjamin Mote                 ,United Kingdom      
+46,John Blackwood                ,United Kingdom      
+47,Heinemann                     ,United Kingdom      
+48,Story                         ,USA                 
+49,Arta                          ,Novosibirsk         
+50,AST                           ,Moscow              
+51,AST                           ,Moscow              
+52,AST                           ,Moscow              
+53,ОАО Луч                       ,Moscow              
+54,Мир                           ,Saint-Petersburg    
+55,Питер софт                    ,Saint-Petersburg    
+56,Наука                         ,Moscow              
+57,ЗАО Квантор                   ,Krakow              
+58,Nauka                         ,Karaganda           
